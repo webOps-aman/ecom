50e3e8647f660ce5031e467ffe2539d384947683
webOps-aman <amanhxe@gmail.com>
2025-02-12 21:21:02 +0530

    modified

--- project/frontend/src/components/Login/Login.jsx
+++ project/frontend/src/components/Login/Login.jsx
@@ -38,7 +38,7 @@ const Login = () => {
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Login to your account
+          Login To Your Account
         </h2>
       </div>
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
