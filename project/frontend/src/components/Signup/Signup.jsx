b8152f9dbd237cc5f1b557adb6ecdf0bf510388c
webOps-aman <amanhxe@gmail.com>
2025-02-16 10:48:15 +0530

    modified

--- project/frontend/src/components/Signup/Signup.jsx
+++ project/frontend/src/components/Signup/Signup.jsx
@@ -71,7 +71,7 @@ const Singup = () => {
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Register as a new user
+          Register As a New User
         </h2>
       </div>
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
